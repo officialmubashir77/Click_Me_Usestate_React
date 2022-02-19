14b2a163c774a7efe8112c53904dd042f76fef54
Mubashir-M-Ibrahim <mubashirm.ibrahim@gmail.com>
2022-02-19 11:32:46 +0500

    react_click_me_usestate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+
+  let [count , setCount] = useState(0)
+
+  console.log(count);
+  let IncValue = () =>  {
+
+    // console.log(count);
+    // count++ ;
+    setCount(++count)
+
+
+  }
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+    <div>
+
+      <h1> {count} </h1>
+
+      <button onClick={IncValue}> Click me </button>
+
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
